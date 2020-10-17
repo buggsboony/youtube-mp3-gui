eeccca487ef92f8c3abbe6771753b6d559487d96
buggsboony <boonydeboon@gmail.com>
2020-10-17 19:07:46 -0400

    install uninstall ok

--- install.sh
+++ install.sh
@@ -6,8 +6,13 @@ extension=.sh
 #peut être extension vide
 
 
-
+#check installed package , is paquage installed ?
+#pacman -Qi youtube-dl | grep -i version
+if [ $(pacman -Qi youtube-dl | grep -i version | wc -l) == 1 ]; then 
+echo "youtube-dl already installed OK"
+else
 sudo pacman -S youtube-dl
+fi
 
 
 
@@ -16,11 +21,11 @@ cp $what.desktop_original $what.desktop
 sed -i "s;HOME_PATH;$HOME;g" $what.desktop
 echo "moving desktop file"
 dest="/usr/share/applications/$what.desktop"
-mv $what.desktop "$dest"
+sudo mv $what.desktop "$dest"
 echo "Creating path"
-mkdir -p /usr/share/youtube-mp3-gui
+sudo mkdir -p /usr/share/youtube-mp3-gui
 echo "Copy icon image"
-cp youtube.png /usr/share/youtube-mp3-gui/
+sudo cp youtube.png /usr/share/youtube-mp3-gui/
 echo "Set executable..."
 chmod +x $what$extension
 #echo "lien symbolique vers usr bin"
--- uninstall.sh
+++ uninstall.sh
@@ -8,11 +8,11 @@ extension=.sh
 echo "killing running instances"
 killall $what
 
-echo "removing symbolic link from usr bin"
-sudo rm /usr/bin/$what
+echo "removing symbolic link"
+sudo rm /usr/local/bin/$what
 echo "removing data folder /usr/share/youtube-mp3-gui"
-sudo rm -rf /usr/share/youtube-mp3-gui
+sudo rm -rf /usr/share/$what
 echo "removing desktop icon"
-sudo rm /usr/share/applications/$what.desktop"
+sudo rm /usr/share/applications/$what.desktop
 echo "done."
 
